67b0236154c39a3ccc2fc0566a00383e5a6f10d5
Nord1107 <umberto-5g@mail.ru>
2024-06-02 16:10:13 +0300

    Added delete function.

--- script/script.js
+++ script/script.js
@@ -1,8 +1,24 @@
-function newElement() {
-    const li = document.createElement("li");
-    const inputValue = document.getElementById("inp1").value;
-    const crTextLi = document.createTextNode(inputValue);
-    li.appendChild(crTextLi);
-    document.getElementById("inp1").value = "";
-    document.getElementById("listToDo").appendChild(li);
+let arrayToDo = [];
+
+function showTodo() {
+    const todo = document.getElementById("listToDo");
+    todo.innerHTML = "";
+    for(let i=0; i < arrayToDo.length; i++) {
+        const li = document.createElement("li");
+        const img = document.createElement("img")
+        img.src = "icons/trash.png";
+        img.className = "trash";
+        li.textContent = arrayToDo[i];
+        li.appendChild(img);
+        todo.appendChild(li);
+        img.onclick = function() {arrayToDo.splice(i, 1); showTodo()};
+    }
 }
+
+function newElement() {
+    const inputValue = document.getElementById("inp1").value;
+    arrayToDo.push(inputValue);
+    document.getElementById("inp1").value = "";
+    showTodo();
+}
+showTodo();
